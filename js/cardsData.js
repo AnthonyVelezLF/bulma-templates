ce7860043f72f15be3e0ceecd974ba78794181ea
Anth8ny LF <avelezg3@unemi.edu.ec>
2024-07-03 17:56:26 -0500

    Added and updated .gitattributes and various CSS files
    subiendo la carpeta del proyecto

--- js/cardsData.js
+++ js/cardsData.js
@@ -1,80 +1,57 @@
 let cardsData = [{
   id: 1,
-  image: 'https://images.unsplash.com/photo-1504173010664-32509aeebb62?ixlib=rb-1.2.1&auto=format&fit=crop&w=713&q=80',
-  avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+  image: 'https://www.intagri.com/assets/images/articulos/categoria1/frutillas/art215-manejo-produccion-zarzamora/cultivo.jpg',
+  avatar: 'https://st.depositphotos.com/16685936/55739/v/450/depositphotos_557394034-stock-illustration-young-smiling-man-farmer-agricultural.jpg',
   user: {
-    name: 'Okinami',
-    handle: 'twitterid',
-    title: 'Lead Developer'
+    name:'',
+    handle: 'Agricultor',
+    title: 'Cultivo'
   },
-  content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
-},
-{
-  id: 1,
-  image: 'https://images.unsplash.com/photo-1687851898832-650714860119?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-  avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
-  user: {
-    name: 'Okinami',
-    handle: 'twitterid',
-    title: 'Lead Developer'
-  },
-  content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  content: 'La mora crece en suelos ácidos, pero se desanolla mejor en suelos que no lo sean, requiere suelos profundos, y es exigente de nitrógeno, fÓsforo, potasio, calcio y magnesio. Los mejores suelos para el cultivo de la mora son los francos, con buena aireación y que no se encharquen.'
 },
 {
   id: 2,
-  image: 'https://images.unsplash.com/photo-1687851898832-650714860119?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-  avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+  image: 'https://lh7-us.googleusercontent.com/IpTa4MmF50HThCUxwV0mDDXcwqefrRWyyPBq6hSGG5g1QzhP2tQDLah7D7P9ObUcyfWgyCXKxBFFEXOrXOzIy3sSUy_KnbmTahDyDmJBhtIl4U9VHUX_LSJmMOOsDssk4DzAkXhWV9VCkx_7Y5PYBgo',
+  avatar: 'https://st.depositphotos.com/1002944/4517/v/600/depositphotos_45174801-stock-illustration-blackberry.jpg',
   user: {
-    name: 'Okinami',
-    handle: 'twitterid',
-    title: 'Lead Developer'
+    name:'',
+    handle: 'Agricultor',
+    title: 'Cosecha'
   },
-  content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  content: 'La cosecha de la mora se lleva a cabo normalmente a los ocho meses desde el trasplante. Se debe realizar cuando la fruta se encuentre en su momento óptimo de maduración, que suele coincidir cuando ésta adquiere un color rojo tinto.'
 },
 {
   id: 3,
-  image: 'https://images.unsplash.com/photo-1687851898832-650714860119?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-  avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+  image: 'https://scontent.fgye18-1.fna.fbcdn.net/v/t39.30808-6/352359330_244230841691073_7457640853079608860_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHe4VCPM2lPcPW85ixC5zoFGmzTJSc69WEabNMlJzr1YeJKQBcMMsvgIlkxTqiLJeDjIE6yT-_kWQgw6Xv_xqS9&_nc_ohc=Tk5XnCsuM00Q7kNvgGzugXk&_nc_ht=scontent.fgye18-1.fna&oh=00_AYBzRRDufzfzeS7JE19zEkW8-l8jcry5gPXgmRU9zUKSsA&oe=667A2072',
+  avatar: 'https://st2.depositphotos.com/50337402/47111/v/600/depositphotos_471112132-stock-illustration-black-delivery-small-truck-side.jpg',
   user: {
-    name: 'Okinami',
-    handle: 'twitterid',
-    title: 'Lead Developer'
+    name:'',
+    handle: 'Transporte',
+    title: 'Transporte y Almacenamiento'
   },
-  content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  content: 'Para transportar moras adecuadamente, deben mantenerse refrigeradas entre 0 y 2 grados Celsius en contenedores rígidos y ventilados para evitar aplastamientos y permitir la circulación de aire. La manipulación debe ser cuidadosa para prevenir daños, evitando amontonarlas y protegiéndolas de golpes.'
 },
 {
   id: 4,
-  image: 'https://images.unsplash.com/photo-1687851898832-650714860119?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-  avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+  image: 'https://images.squarespace-cdn.com/content/v1/58597a19e58c62ba7fa48bf9/1571794436162-7D69R6WRRDTVHETKXSDO/unnamed.jpg',
+  avatar: 'https://st.depositphotos.com/1507819/3933/v/600/depositphotos_39338009-stock-illustration-industrial-icon.jpg',
   user: {
-    name: 'Okinami',
-    handle: 'twitterid',
-    title: 'Lead Developer'
+    handle: 'Industria',
+    title: 'Procesamiento para Obtener la Pulpa'
   },
-  content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  content: 'Para obtener la pulpa de mora a nivel industrial, primero se seleccionan y lavan las moras. Luego, se trituran y despulpan para separar la pulpa de semillas y cáscaras. La pulpa se homogeniza para lograr una consistencia uniforme y se pasteuriza para eliminar microorganismos. Finalmente, se envasa de manera aséptica para garantizar su frescura y seguridad.'
 },
 {
   id: 5,
-  image: 'https://images.unsplash.com/photo-1687851898832-650714860119?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-  avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+  image: 'https://images.squarespace-cdn.com/content/v1/59a435d9d55b41551e28184b/1585234990387-FUJ0ZVAE4KT6BCJ2SLQH/covid-19-5-tips-para-planificar-eficazmente-la-distribucion-de-productos.jpg',
+  avatar: 'https://st3.depositphotos.com/17553142/36336/v/600/depositphotos_363368702-stock-illustration-worker-hardhat-helmet-construction-builder.jpg',
   user: {
-    name: 'Okinami',
-    handle: 'twitterid',
-    title: 'Lead Developer'
+    handle: 'Distribuicion',
+    title: 'Almacenamiento y Distribución'
   },
-  content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
-},
-{
-  id: 6,
-  image: 'https://ucarecdn.com/83e71ac6-6c75-4e7c-96e9-5aae15a4b3f0/',
-  avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
-  user: {
-    name: 'Okinami',
-    handle: 'twitterid',
-    title: 'Lead Developer'
-  },
-  content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
-},
+  content: 'La pulpa de mora debe almacenarse a -18°C o menos y distribuirse en camiones refrigerados para mantener la cadena de frío. Es esencial monitorear la temperatura constantemente para preservar la calidad y frescura del producto hasta su uso o venta.'
+}
+
 ]
 
 
